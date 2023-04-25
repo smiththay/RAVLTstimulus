8c303028df22de0908dbd548e7d674e64cfdfc74
Smith Thay <77938144+smiththay@users.noreply.github.com>
2023-04-25 16:55:20 -0400

    Initial commit

---
+++ controllers/recall_controller.js
@@ -0,0 +1,26 @@
+﻿// src/controllers/hello_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static targets = ['trial1', 'trial2', 'totalRecall']
+
+    updateTotalRecall() {
+        const totalRecall = this.trial1() + this.trial2()
+        this.totalRecallTarget.value = Number(totalRecall)
+    }
+
+    trial1() {
+        return parseInt(this.trial1Target.value || 0)
+    }
+
+    trial2() {
+        return parseInt(this.trial2Target.value || 0)
+    }
+
+    totalRecall() {
+        return parseInt(this.totalRecallTarget.value)
+    }
+
+    console.log('hello')
+}
+
